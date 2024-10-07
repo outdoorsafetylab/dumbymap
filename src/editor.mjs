b761821e1ad50b42c92e1bbb7926bed3f3195fac
Hsieh Chin Fan <pham@topo.tw>
2024-10-07 17:44:30 +0800

    fix: only show "More" with renderer suggestions in menu

--- src/editor.mjs
+++ src/editor.mjs
@@ -776,14 +776,16 @@ const getSuggestions = anchor => {
             cm
           })
       )
-    return [
-      ...(rendererSuggestions ?? []),
-      new menuItem.Item({
-        innerHTML: '<a href="https://github.com/outdoorsafetylab/mapclay#renderer" class="external" style="display: block;">More...</a>',
-        className: ['suggestion'],
-        onclick: () => window.open('https://github.com/outdoorsafetylab/mapclay#renderer', '_blank')
-      })
-    ]
+    return rendererSuggestions.length === 0
+      ? []
+      : [
+        ...rendererSuggestions,
+        new menuItem.Item({
+          innerHTML: '<a href="https://github.com/outdoorsafetylab/mapclay#renderer" class="external" style="display: block;">More...</a>',
+          className: ['suggestion'],
+          onclick: () => window.open('https://github.com/outdoorsafetylab/mapclay#renderer', '_blank')
+        })
+      ]
   }
   return []
 }
