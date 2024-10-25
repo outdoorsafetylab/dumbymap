c31f0c502808848bb343646c5e78795b9d5a17ee
Hsieh Chin Fan <pham@topo.tw>
2024-10-25 12:10:00 +0800

    chore: improve scripts for gh-pages

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+origin_branch=`git branch --show-current`
+
 git checkout gh-pages -- .gitignore index.html && \
 npm run build && npm run docs && \
 git branch --force gh-pages HEAD && \
 git checkout gh-pages && \
 git add --all && git commit -m gh-pages && \
 git push -f &&
-git checkout HEAD^
+git checkout $origin_branch
