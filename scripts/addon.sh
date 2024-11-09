fe80b044492ad6bf5015c11cd3c76a5fdd537c8c
Hsieh Chin Fan <pham@topo.tw>
2024-11-09 17:49:07 +0800

    chore: remove extra line in addon script

--- scripts/addon.sh
+++ scripts/addon.sh
@@ -4,5 +4,4 @@ mkdir -p addon/css
 ln -f src/css/dumbymap.css addon/css/dumbymap.css
 
 mkdir -p addon/scripts
-rollup --format=iife addon/src/dumbymap.mjs --dir addon/scripts
 ls addon/src/* | xargs -i rollup --format=iife {} --dir addon/scripts
