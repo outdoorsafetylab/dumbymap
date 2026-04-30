aecb3d8a8e9edcf4c8ff9053a1b51789151583ef
Hsieh Chin Fan <pham@topo.tw>
2026-04-30 13:02:59 +0800

    chore: add playwright/test into dependencies and minor config change

--- scripts/playwright.config.js
+++ scripts/playwright.config.js
@@ -3,6 +3,11 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: '../test/e2e',
   timeout: 30000,
+  webServer: {
+    command: 'npx serve -l 8888 .',
+    url: 'http://localhost:8888',
+    reuseExistingServer: true,
+  },
   use: {
     ...devices['Desktop Chrome'],
     baseURL: 'http://localhost:8888',
